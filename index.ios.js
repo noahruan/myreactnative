ec2e89226c35e8ce746553f6e3b311414cc32450
noahruan <noahruan@126.com>
2016-03-16 17:12:43 +0800

    movielistdemo

--- index.ios.js
+++ index.ios.js
@@ -8,23 +8,70 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  Image,
+  View,
+  ListView
 } from 'react-native';
 
+var REQUEST_URL='https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 class mynewrn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //自定义的state变量及初始值
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 != row2
+      }),
+      loaded: false
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    fetch(REQUEST_URL)
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+            loaded: true
+          });
+        })
+        .done();
+  }
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return(
+        <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderMovie}
+            style={styles.listView}
+            />
+    );
+  }
+
+  renderLoadingView() {
+    return (
+        <View style={styles.container}>
+          <Text>
+            正在加载电影数据……
+          </Text>
+        </View>
+    );
+  }
+  renderMovie(movie) {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image source={{uri: movie.posters.thumbnail}}
+               style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -33,20 +80,30 @@ class mynewrn extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
+  rightContainer: {
+    flex: 1
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  },
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 8,
+    textAlign: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  year: {
+    textAlign: 'center'
   },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF'
+  }
 });
 
 AppRegistry.registerComponent('mynewrn', () => mynewrn);
